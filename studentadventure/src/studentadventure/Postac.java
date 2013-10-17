e8d419a1a969ca377317c5468e93cac1fd0a0909
azranel <chunkyo@gmail.com>
2013-10-17 22:19:52 +0200

    Poprawione. Nie ma już mvp i exp.

--- studentadventure/src/studentadventure/Postac.java
+++ studentadventure/src/studentadventure/Postac.java
@@ -4,11 +4,9 @@ public abstract class Postac {
 private String description; //opis postaci
 private String name; // nazwa
 private int hp; // punkty zycia
-private int exp; // punkty doswiadczenia
-private int mvp; // punkty ruchu
 
 public String toString() {
-	return "<" + name + " , " + hp + " , " + mvp + " , " + exp + ">";
+	return "<" + name + " , " + hp + ">";
 }
 public int getHp() {
 	return hp;
@@ -16,18 +14,7 @@ public int getHp() {
 public void setHp(int hp) {
 	this.hp = hp;
 }
-public int getExp() {
-	return exp;
-}
-public void setExp(int exp) {
-	this.exp = exp;
-}
-public int getMvp() {
-	return mvp;
-}
-public void setMvp(int mvp) {
-	this.mvp = mvp;
-}
+
 public String getDescription() {
 	return description;
 }
