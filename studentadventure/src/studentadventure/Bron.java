c5f3bdc1515177b8f2299877eae6fa9a1f2a0173
Marcin <marcin.czainski@o2.pl>
2013-10-18 10:45:35 +0200

    Update Bron.java

--- studentadventure/src/studentadventure/Bron.java
+++ studentadventure/src/studentadventure/Bron.java
@@ -7,10 +7,14 @@ public abstract class Bron
 	private int level; //poziom broni
 	private int sila_ataku; // współczynnik modyfikujący atak bohatera
 	private int sila_obrony; // współczynnik modyfikujący obronę bohatera
+	
 	public String toString() { return "Bron: " + name + "ma poziom: " + level; } 
+	
 	public void setName(String name) { this.name = name; } 
 	public String getName() { return name; } 
+	
 	public void setDescription(String description) { this.description = description; } 
 	public String getDescription() { return description; } 
+	
 	public String about() { return name + " to " + description; }
 }
