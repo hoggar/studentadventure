fdbd57e729ce680c5663570e630d2275e5b6a37b
Marcin <marcin.czainski@o2.pl>
2013-10-18 10:44:07 +0200

    Update Bron.java

--- studentadventure/src/studentadventure/Bron.java
+++ studentadventure/src/studentadventure/Bron.java
@@ -1,18 +1,14 @@
-public static void main (String[] args) throws java.lang.Exception
+public abstract class Bron 
 {
-public abstract class Bron { private String name; // nazwa broni 
-private String description; //opis broni
-private int level; //poziom broni
-private int sila_ataku // współczynnik modyfikujący atak bohatera
-private int sila_obrony // współczynnik modyfikujący obronę bohatera
-public String toString() { return "Bron: " + name + "ma poziom: " + level 
-". Współczynnik ataku" + sila_ataku "Współczynnik obrony" + sila_obrony; } 
-public void setName(String name) { this.name = name; } 
-public String getName() { return name; } 
-public void setDescription(String description) { this.description = description; } 
-public String getDescription() { return description; } 
-public String about() { return name + " to " + description; }
-
-}
-}
+	private String name; // nazwa broni 
+	private String description; //opis broni
+	private int level; //poziom broni
+	private int sila_ataku; // współczynnik modyfikujący atak bohatera
+	private int sila_obrony; // współczynnik modyfikujący obronę bohatera
+	public String toString() { return "Bron: " + name + "ma poziom: " + level; } 
+	public void setName(String name) { this.name = name; } 
+	public String getName() { return name; } 
+	public void setDescription(String description) { this.description = description; } 
+	public String getDescription() { return description; } 
+	public String about() { return name + " to " + description; }
 }
