f2fa3aa1d2ffc447641df44651e669c51bb9b6f9
Karolina <Karolina@Kara>
2014-01-23 23:18:54 +0100

    update przedmotow klasy i te sprawy

--- studentadventure/src/studentadventure/Przedmiot.java
+++ studentadventure/src/studentadventure/Przedmiot.java
@@ -3,8 +3,22 @@ package studentadventure;
 public class Przedmiot {
 	protected String nazwa; // nazwa przedmiotu
 	protected String opis; //opis przedmiotu
-	protected int level; //poziom przedmiotu
 	
+	protected int level; //poziom przedmiotu
+	protected int x; //wspolrzedna x przedmiotu na mapie
+	protected int y; // wspolrzedna y przedmiotu na mapie
+	
+	
+public Przedmiot(int x2, int y2, String nazwa2, String opis2) {
+		x=x2;
+		y=y2;
+		nazwa=nazwa2;
+		opis=opis2;
+	
+	}
+public Przedmiot() {
+	// TODO Auto-generated constructor stub
+}
 public String toString() {
 	return "Przedmiot: " + nazwa + " ma poziom:  " + level;
 	}
